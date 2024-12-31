fb0ce6f5b1d11175abf44b8fc2a217413af32ecc
VishAyush <139580532+INFERNO1211@users.noreply.github.com>
2024-12-31 22:58:42 +0530

    added frontend

--- backend/handlers/verify.go
+++ backend/handlers/verify.go
@@ -12,7 +12,7 @@ func VerifyHandler(w http.ResponseWriter, r *http.Request) {
 
 	
 	// Set CORS headers
-	w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+	w.Header().Set("Access-Control-Allow-Origin", "https://email-verification-app-gamma.vercel.app/")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
